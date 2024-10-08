b988757f08d950f81afd76b5fe0e349899838dd3
AEAA17 <euricio.alexandre@gmail.com>
2024-10-08 14:07:09 -0300

    Update Deploy.py

--- Deploy.py
+++ Deploy.py
@@ -46,7 +46,7 @@ if botao:
     dic.update(x_numericos)
     dic.update(x_tf)
     valores_x = pd.DataFrame([dic])  # Criando o DataFrame com os valores de entrada
-    dados = pd.read_csv(r"https://github.com/AEAA17/Deployairbnb/blob/main/dados.csv")
+    dados = pd.read_csv(r"https://raw.githubusercontent.com/AEAA17/Deployairbnb/refs/heads/main/dados.csv")
     colunas = list(dados.columns)[1:-1]
     valores_x = valores_x[colunas]
     modelo = joblib.load(r"C:\Users\euric\Documents\Códigos Py\PDC\modelo.joblib")
